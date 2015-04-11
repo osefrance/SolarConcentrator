019ffd4174653f4e767db96af894a2e8978ad706
Andrea Sannuto <andrea.sannuto@gmail.com>
2015-04-11 03:56:43 +0200

    added comments.
    it features a simple microstepping of 1/16th of 200 full steps stepper motor and makes a microstep every 54seconds or so.
    
    will need to add:
    
    release 1:
    - reset to 0 position using an optical sensor
    - first setup on the morning (need rtc implementation) using 2 light sensors on the absorber edges
    - last setup on the evening in order to understand the error and use the new delay time for the day after.
    release 2:
    - multiple mirrors using an IC (need wire implementation)
    release 3:
    - safety shut off of the mirrors
    - statistical information
    ...

--- heliostat/SeguiLSol25/SeguiLSol25.ino
+++ heliostat/SeguiLSol25/SeguiLSol25.ino
@@ -9,6 +9,23 @@
 // this software is released under the GPL V.3 
 // for more recents update, check out the git page: https://github.com/osefrance/SolarConcentrator
 //**************************************************************
+/*
+it features a simple microstepping of 1/16th of 200 full steps stepper motor and makes a microstep every 54seconds or so.
+
+will need to add:
+
+release 1:
+- reset to 0 position using an optical sensor
+- first setup on the morning (need rtc implementation) using 2 light sensors on the absorber edges
+- last setup on the evening in order to understand the error and use the new delay time for the day after.
+release 2:
+- multiple mirrors using an IC (need wire implementation)
+release 3:
+- safety shut off of the mirrors
+- statistical information
+...
+*/
+
 
 // include : soon wire, rtc
 //  #include 
