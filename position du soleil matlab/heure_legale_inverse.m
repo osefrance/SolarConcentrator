0b45ef495c081ccd7e6103581af4e22f0bf383f4
ooriane <oriane.mollet@sfr.fr>
2015-06-21 16:28:17 +0200

    fichier manquant
    prog matlab passe de l'heure légale décimale à l'heure solaire

---
+++ position du soleil matlab/heure_legale_inverse.m
@@ -0,0 +1,23 @@
+function t=heure_legale_inverse(n,h,long)
+% heure legale moyenne a partir de l'heure solaire vraie
+% long longitude en degres decimaux (negative a l'est de greenwich)
+% n numero du jour [1:365]
+% t heure solaire
+% h heure légale décimale
+% correction de l'equation du temps (formule simple)
+% irregularites dans la rotation de la Terre
+va=2*pi/365; % vitesse angulaire de la Terre (rad/jour)
+l=va*(n-81);
+E=-9.87*sin(2*l)+7.53*cos(l)+1.5*sin(l); % en minutes decimales
+
+% correction de longitude 
+Long=4*long; % en minutes décimales
+
+% ajouter aussi heure d'ete +2 - heure d'hiver +1 (valeurs pour 2015)
+if n<88 || n>297
+    u=1;
+else u=2;
+end
+
+t=h-u-(E+Long)/60;
+end
